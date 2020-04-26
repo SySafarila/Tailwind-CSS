62ac2d67282a66e5c66a59a8fae58b838bd12498
Syahrul Safarila <sysafarila.official@gmail.com>
2020-04-26 09:08:39 +0700

    Update UI

--- script/script.js
+++ script/script.js
@@ -3,7 +3,17 @@ const rightButton = document.getElementById('rightButton');
 function rightButtonn() {
     if (rightButton.innerHTML == 'menu') {
         rightButton.innerHTML = 'menu_open';
+        show();
     } else {
         rightButton.innerHTML = 'menu';
+        hidden();
     }
+}
+
+function show() {
+    document.getElementById('show').className = "";
+}
+
+function hidden() {
+    document.getElementById('show').className = "hidden";
 }
