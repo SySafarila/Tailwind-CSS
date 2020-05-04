cbe9ec4f90a17fbe9efdc5ef567229a348d9729f
Syahrul Safarila <sysafarila.official@gmail.com>
2020-05-04 14:35:00 +0700

    Update UI

--- script/script.js
+++ script/script.js
@@ -21,11 +21,16 @@ function hidden() {
 // New
 function clickUl(){
     const nav = document.getElementById('ul-navbar');
+    const menu = document.getElementById('menu');
     if (nav.classList.contains('scale-y-0') == true) {
         // console.log('true');
+        menu.innerHTML = 'menu_open';
+        menu.classList.add('rotate-180');
         nav.classList.remove('scale-y-0');
     } else {
         // console.log('false');
+        menu.innerHTML = 'menu';
+        menu.classList.remove('rotate-180');
         nav.classList.add('scale-y-0');
     }
 }
